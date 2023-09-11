bb8037a1358f3f9e5cc99b9011bd87ad75b4f588
1kaiser <schlboy26@gmail.com>
2023-09-12 00:49:04 +0530

    Update WRF-MOSIT.sh
    changed links

--- WRF-MOSIT.sh
+++ WRF-MOSIT.sh
@@ -2148,11 +2148,11 @@ if [ "$Ubuntu_64bit_Intel" = "1" ] && [ "$WRFHYDRO_STANDALONE_PICK" = "1" ]; the
   wget -c https://github.com/HDFGroup/hdf5/archive/refs/tags/hdf5-1_14_1-2.tar.gz
   wget -c https://github.com/Unidata/netcdf-c/archive/refs/tags/v4.9.2.tar.gz
   wget -c https://github.com/Unidata/netcdf-fortran/archive/refs/tags/v4.6.0.tar.gz
-  wget -c https://download.sourceforge.net/libpng/libpng-1.6.39.tar.gz
+  wget -c https://github.com/1kaiser/WRF-MOSIT/releases/download/1/libpng-1.6.40.tar.gz
   wget -c https://www.ece.uvic.ca/~frodo/jasper/software/jasper-1.900.1.zip
   wget -c https://github.com/pmodels/mpich/releases/download/v4.1.2/mpich-4.1.2.tar.gz
   wget -c https://github.com/1kaiser/WRF-MOSIT/releases/download/1/pnetcdf-1.12.3.tar.gz
-  wget -c https://sourceforge.net/projects/opengrads/files/grads2/2.2.1.oga.1/Linux%20%2864%20Bits%29/opengrads-2.2.1.oga.1-bundle-x86_64-pc-linux-gnu-glibc_2.17.tar.gz
+  wget -c https://github.com/1kaiser/WRF-MOSIT/releases/download/1/opengrads-2.2.1.oga.1-bundle-x86_64-pc-linux-gnu-glibc_2.17.tar.gz
 
 
 
@@ -3648,11 +3648,11 @@ if [ "$Ubuntu_64bit_Intel" = "1" ] && [ "$WRFHYDRO_COUPLED_PICK" = "1" ]; then
   wget -c https://github.com/HDFGroup/hdf5/archive/refs/tags/hdf5-1_14_1-2.tar.gz
   wget -c https://github.com/Unidata/netcdf-c/archive/refs/tags/v4.9.2.tar.gz
   wget -c https://github.com/Unidata/netcdf-fortran/archive/refs/tags/v4.6.0.tar.gz
-  wget -c https://download.sourceforge.net/libpng/libpng-1.6.39.tar.gz
+  wget -c https://github.com/1kaiser/WRF-MOSIT/releases/download/1/libpng-1.6.40.tar.gz
   wget -c https://www.ece.uvic.ca/~frodo/jasper/software/jasper-1.900.1.zip
   wget -c https://github.com/pmodels/mpich/releases/download/v4.1.2/mpich-4.1.2.tar.gz
   wget -c https://github.com/1kaiser/WRF-MOSIT/releases/download/1/pnetcdf-1.12.3.tar.gz
-  wget -c https://sourceforge.net/projects/opengrads/files/grads2/2.2.1.oga.1/Linux%20%2864%20Bits%29/opengrads-2.2.1.oga.1-bundle-x86_64-pc-linux-gnu-glibc_2.17.tar.gz
+  wget -c https://github.com/1kaiser/WRF-MOSIT/releases/download/1/opengrads-2.2.1.oga.1-bundle-x86_64-pc-linux-gnu-glibc_2.17.tar.gz
 
 
   echo " "
@@ -4522,11 +4522,11 @@ if [ "$macos_64bit_GNU" = "1" ] && [ "$WRFHYDRO_COUPLED_PICK" = "1" ]; then
     wget -c https://github.com/HDFGroup/hdf5/archive/refs/tags/hdf5-1_14_1-2.tar.gz
     wget -c https://github.com/Unidata/netcdf-c/archive/refs/tags/v4.9.2.tar.gz
     wget -c https://github.com/Unidata/netcdf-fortran/archive/refs/tags/v4.6.0.tar.gz
-    wget -c https://download.sourceforge.net/libpng/libpng-1.6.39.tar.gz
+    wget -c https://github.com/1kaiser/WRF-MOSIT/releases/download/1/libpng-1.6.40.tar.gz
     wget -c https://www.ece.uvic.ca/~frodo/jasper/software/jasper-1.900.1.zip
     wget -c https://github.com/pmodels/mpich/releases/download/v4.1.2/mpich-4.1.2.tar.gz
     wget -c https://github.com/1kaiser/WRF-MOSIT/releases/download/1/pnetcdf-1.12.3.tar.gz
-    wget -c https://sourceforge.net/projects/opengrads/files/grads2/2.2.1.oga.1/Linux%20%2864%20Bits%29/opengrads-2.2.1.oga.1-bundle-x86_64-pc-linux-gnu-glibc_2.17.tar.gz
+    wget -c https://github.com/1kaiser/WRF-MOSIT/releases/download/1/opengrads-2.2.1.oga.1-bundle-x86_64-pc-linux-gnu-glibc_2.17.tar.gz
 
 
 
@@ -5262,11 +5262,11 @@ if [ "$Ubuntu_64bit_GNU" = "1" ] && [ "$WRFCHEM_PICK" = "1" ]; then
   wget -c https://github.com/HDFGroup/hdf5/archive/refs/tags/hdf5-1_14_1-2.tar.gz
   wget -c https://github.com/Unidata/netcdf-c/archive/refs/tags/v4.9.2.tar.gz
   wget -c https://github.com/Unidata/netcdf-fortran/archive/refs/tags/v4.6.0.tar.gz
-  wget -c https://download.sourceforge.net/libpng/libpng-1.6.39.tar.gz
+  wget -c https://github.com/1kaiser/WRF-MOSIT/releases/download/1/libpng-1.6.40.tar.gz
   wget -c https://www.ece.uvic.ca/~frodo/jasper/software/jasper-1.900.1.zip
   wget -c https://github.com/pmodels/mpich/releases/download/v4.1.2/mpich-4.1.2.tar.gz
   wget -c https://github.com/1kaiser/WRF-MOSIT/releases/download/1/pnetcdf-1.12.3.tar.gz
-  wget -c https://sourceforge.net/projects/opengrads/files/grads2/2.2.1.oga.1/Linux%20%2864%20Bits%29/opengrads-2.2.1.oga.1-bundle-x86_64-pc-linux-gnu-glibc_2.17.tar.gz
+  wget -c https://github.com/1kaiser/WRF-MOSIT/releases/download/1/opengrads-2.2.1.oga.1-bundle-x86_64-pc-linux-gnu-glibc_2.17.tar.gz
 
 
   echo " "
@@ -6350,11 +6350,11 @@ if [ "$Ubuntu_64bit_Intel" = "1" ] && [ "$WRFCHEM_PICK" = "1" ]; then
   wget -c https://github.com/HDFGroup/hdf5/archive/refs/tags/hdf5-1_14_1-2.tar.gz
   wget -c https://github.com/Unidata/netcdf-c/archive/refs/tags/v4.9.2.tar.gz
   wget -c https://github.com/Unidata/netcdf-fortran/archive/refs/tags/v4.6.0.tar.gz
-  wget -c https://download.sourceforge.net/libpng/libpng-1.6.39.tar.gz
+  wget -c https://github.com/1kaiser/WRF-MOSIT/releases/download/1/libpng-1.6.40.tar.gz
   wget -c https://www.ece.uvic.ca/~frodo/jasper/software/jasper-1.900.1.zip
   wget -c https://github.com/pmodels/mpich/releases/download/v4.1.2/mpich-4.1.2.tar.gz
   wget -c https://github.com/1kaiser/WRF-MOSIT/releases/download/1/pnetcdf-1.12.3.tar.gz
-  wget -c https://sourceforge.net/projects/opengrads/files/grads2/2.2.1.oga.1/Linux%20%2864%20Bits%29/opengrads-2.2.1.oga.1-bundle-x86_64-pc-linux-gnu-glibc_2.17.tar.gz
+  wget -c https://github.com/1kaiser/WRF-MOSIT/releases/download/1/opengrads-2.2.1.oga.1-bundle-x86_64-pc-linux-gnu-glibc_2.17.tar.gz
 
 
   echo " "
@@ -7179,11 +7179,11 @@ if [ "$macos_64bit_GNU" = "1" ] && [ "$WRFCHEM_PICK" = "1" ]; then
     wget -c https://github.com/HDFGroup/hdf5/archive/refs/tags/hdf5-1_14_1-2.tar.gz
     wget -c https://github.com/Unidata/netcdf-c/archive/refs/tags/v4.9.2.tar.gz
     wget -c https://github.com/Unidata/netcdf-fortran/archive/refs/tags/v4.6.0.tar.gz
-    wget -c https://download.sourceforge.net/libpng/libpng-1.6.39.tar.gz
+    wget -c https://github.com/1kaiser/WRF-MOSIT/releases/download/1/libpng-1.6.40.tar.gz
     wget -c https://www.ece.uvic.ca/~frodo/jasper/software/jasper-1.900.1.zip
     wget -c https://github.com/pmodels/mpich/releases/download/v4.1.2/mpich-4.1.2.tar.gz
     wget -c https://github.com/1kaiser/WRF-MOSIT/releases/download/1/pnetcdf-1.12.3.tar.gz
-    wget -c https://sourceforge.net/projects/opengrads/files/grads2/2.2.1.oga.1/Linux%20%2864%20Bits%29/opengrads-2.2.1.oga.1-bundle-x86_64-pc-linux-gnu-glibc_2.17.tar.gz
+    wget -c https://github.com/1kaiser/WRF-MOSIT/releases/download/1/opengrads-2.2.1.oga.1-bundle-x86_64-pc-linux-gnu-glibc_2.17.tar.gz
 
 
 
@@ -7939,11 +7939,11 @@ if [ "$Ubuntu_64bit_GNU" = "1" ] && [ "$WRF_PICK" = "1" ]; then
   wget -c https://github.com/HDFGroup/hdf5/archive/refs/tags/hdf5-1_14_1-2.tar.gz
   wget -c https://github.com/Unidata/netcdf-c/archive/refs/tags/v4.9.2.tar.gz
   wget -c https://github.com/Unidata/netcdf-fortran/archive/refs/tags/v4.6.0.tar.gz
-  wget -c https://download.sourceforge.net/libpng/libpng-1.6.39.tar.gz
+  wget -c https://github.com/1kaiser/WRF-MOSIT/releases/download/1/libpng-1.6.40.tar.gz
   wget -c https://www.ece.uvic.ca/~frodo/jasper/software/jasper-1.900.1.zip
   wget -c https://github.com/pmodels/mpich/releases/download/v4.1.2/mpich-4.1.2.tar.gz
   wget -c https://github.com/1kaiser/WRF-MOSIT/releases/download/1/pnetcdf-1.12.3.tar.gz
-  wget -c https://sourceforge.net/projects/opengrads/files/grads2/2.2.1.oga.1/Linux%20%2864%20Bits%29/opengrads-2.2.1.oga.1-bundle-x86_64-pc-linux-gnu-glibc_2.17.tar.gz
+  wget -c https://github.com/1kaiser/WRF-MOSIT/releases/download/1/opengrads-2.2.1.oga.1-bundle-x86_64-pc-linux-gnu-glibc_2.17.tar.gz
 
 
 
@@ -9044,11 +9044,11 @@ if [ "$Ubuntu_64bit_Intel" = "1" ] && [ "$WRF_PICK" = "1" ]; then
   wget -c https://github.com/HDFGroup/hdf5/archive/refs/tags/hdf5-1_14_1-2.tar.gz
   wget -c https://github.com/Unidata/netcdf-c/archive/refs/tags/v4.9.2.tar.gz
   wget -c https://github.com/Unidata/netcdf-fortran/archive/refs/tags/v4.6.0.tar.gz
-  wget -c https://download.sourceforge.net/libpng/libpng-1.6.39.tar.gz
+  wget -c https://github.com/1kaiser/WRF-MOSIT/releases/download/1/libpng-1.6.40.tar.gz
   wget -c https://www.ece.uvic.ca/~frodo/jasper/software/jasper-1.900.1.zip
   wget -c https://github.com/pmodels/mpich/releases/download/v4.1.2/mpich-4.1.2.tar.gz
   wget -c https://github.com/1kaiser/WRF-MOSIT/releases/download/1/pnetcdf-1.12.3.tar.gz
-  wget -c https://sourceforge.net/projects/opengrads/files/grads2/2.2.1.oga.1/Linux%20%2864%20Bits%29/opengrads-2.2.1.oga.1-bundle-x86_64-pc-linux-gnu-glibc_2.17.tar.gz
+  wget -c https://github.com/1kaiser/WRF-MOSIT/releases/download/1/opengrads-2.2.1.oga.1-bundle-x86_64-pc-linux-gnu-glibc_2.17.tar.gz
 
 
   echo " "
@@ -9909,11 +9909,11 @@ if [ "$macos_64bit_GNU" = "1" ] && [ "$WRF_PICK" = "1" ]; then
       wget -c https://github.com/HDFGroup/hdf5/archive/refs/tags/hdf5-1_14_1-2.tar.gz
       wget -c https://github.com/Unidata/netcdf-c/archive/refs/tags/v4.9.2.tar.gz
       wget -c https://github.com/Unidata/netcdf-fortran/archive/refs/tags/v4.6.0.tar.gz
-      wget -c https://download.sourceforge.net/libpng/libpng-1.6.39.tar.gz
+      wget -c https://github.com/1kaiser/WRF-MOSIT/releases/download/1/libpng-1.6.40.tar.gz
       wget -c https://www.ece.uvic.ca/~frodo/jasper/software/jasper-1.900.1.zip
       wget -c https://github.com/pmodels/mpich/releases/download/v4.1.2/mpich-4.1.2.tar.gz
       wget -c https://github.com/1kaiser/WRF-MOSIT/releases/download/1/pnetcdf-1.12.3.tar.gz
-      wget -c https://sourceforge.net/projects/opengrads/files/grads2/2.2.1.oga.1/Linux%20%2864%20Bits%29/opengrads-2.2.1.oga.1-bundle-x86_64-pc-linux-gnu-glibc_2.17.tar.gz
+      wget -c https://github.com/1kaiser/WRF-MOSIT/releases/download/1/opengrads-2.2.1.oga.1-bundle-x86_64-pc-linux-gnu-glibc_2.17.tar.gz
 
 
 
@@ -10740,11 +10740,11 @@ if [ "$HWRF_PICK" = "1" ]; then
   wget -c https://github.com/HDFGroup/hdf5/archive/refs/tags/hdf5-1_14_1-2.tar.gz
   wget -c https://github.com/Unidata/netcdf-c/archive/refs/tags/v4.9.2.tar.gz
   wget -c https://github.com/Unidata/netcdf-fortran/archive/refs/tags/v4.6.0.tar.gz
-  wget -c https://download.sourceforge.net/libpng/libpng-1.6.39.tar.gz
+  wget -c https://github.com/1kaiser/WRF-MOSIT/releases/download/1/libpng-1.6.40.tar.gz
   wget -c https://www.ece.uvic.ca/~frodo/jasper/software/jasper-1.900.1.zip
   wget -c https://github.com/pmodels/mpich/releases/download/v4.1.2/mpich-4.1.2.tar.gz
   wget -c https://github.com/1kaiser/WRF-MOSIT/releases/download/1/pnetcdf-1.12.3.tar.gz
-  wget -c https://sourceforge.net/projects/opengrads/files/grads2/2.2.1.oga.1/Linux%20%2864%20Bits%29/opengrads-2.2.1.oga.1-bundle-x86_64-pc-linux-gnu-glibc_2.17.tar.gz
+  wget -c https://github.com/1kaiser/WRF-MOSIT/releases/download/1/opengrads-2.2.1.oga.1-bundle-x86_64-pc-linux-gnu-glibc_2.17.tar.gz
   wget -c https://github.com/Reference-LAPACK/lapack/archive/refs/tags/v3.10.1.tar.gz
 
   wget -c https://dtcenter.org/sites/default/files/HWRF_v4.0a_hwrf-utilities.tar.gz
